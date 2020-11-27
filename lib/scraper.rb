dfd39215b1e39b8832baeca7d6f0ffabd8828851
Mnkith <munkithabid@gmail.com>
2020-11-26 21:18:08 -0600

    commit tree 503146d8d92a61092e5a50bd96342cb40aa086cf

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+courses = doc.css(".title-oE5vT4")
+ courses
+courses.each do |course|
+  puts course.text.strip
+end 
